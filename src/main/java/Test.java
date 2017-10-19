afcf09c0c2dc6e9396b549a08963c031ed9e017d
Kushn_000 <sergey.kushnir@outlook.com>
2017-10-19 18:56:37 +0300

    class TestSample added

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+import com.google.common.math.IntMath;
+
+/**
+ * Created by Kushn_000 on 19.10.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(IntMath.pow(10,3));
+    }
+}
---
+++ src/test/java/TestSample.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+
+/**
+ * Created by Kushn_000 on 19.10.2017.
+ */
+public class TestSample {
+    public static void main(String[] args) {
+        Assert.assertTrue(5 == 10);
+    }
+}
